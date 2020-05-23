14cd02e8998dfcd5bc6e71aefcaf4ea734d1a5a1
sethsaperstein <sethsaper@gmail.com>
2020-05-23 17:14:52 -0400

    - remove references to login and redux for login

--- bexh-web-app/src/redux/index.js
+++ bexh-web-app/src/redux/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import betReducer from '../app/pages/Event/containers/Bets/betReducer';
-import loginReducer from '../app/pages/Login/containers/LoginForm/loginReducer';
 
 export default combineReducers({
     bets: betReducer,
-    login: loginReducer,
 });
--- bexh-web-app/src/redux/types.js
+++ bexh-web-app/src/redux/types.js
@@ -1,5 +1,3 @@
 export const FETCH_BETS = 'FETCH_BETS';
 export const FETCH_MORE_BETS = 'FETCH_MORE_BETS';
 export const NEW_BET = 'NEW_BET';
-
-export const ATTEMPT_LOGIN = 'ATTEMPT_LOGIN';
