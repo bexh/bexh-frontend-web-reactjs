f63ffae96fb6663e11e393970f15037edf5ac7ca
sethsaperstein <sethsaper@gmail.com>
2020-05-30 13:56:25 -0400

    messing with svg and lines

--- bexh-web-app/src/app/components/EventGraph/EventGraph.js
+++ bexh-web-app/src/app/components/EventGraph/EventGraph.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import './style.scss';
 
 export default class EventGraph extends React.Component {
+    constructor(props) {
+        super(props);
+        this.updateSize = this.updateSize.bind(this);
+    }
+
+    // componentDidMount() {
+    //     this.div.addEventListener('resize', this.updateSize);
+    // }
+
+    // componentWillUnmount() {
+    //     this.div.removeEventListener('resize', this.updateSize);
+    // }
+
+    updateSize() {
+        console.log("SIZE", this.div);
+    }
+
     render() {
+        this.updateSize();
         return (
-            <h1>EventGraph</h1>
+            <div ref={div => (this.div = div)} className="eventGraph__container">
+                <svg className="eventGraph__svg" preserveAspectRatio="xMinYMin meet">
+                    <polyline
+                        className="eventGraph__svg"
+                        fill="none"
+                        stroke="#0074d9"
+                        points="
+                        00,00
+                        20,60
+                        40,80
+                        60,20
+                        80,80
+                        100,80
+                        120,60
+                        140,100
+                        160,90
+                        "
+                    />
+                </svg>
+            </div>
         );
     }
 }
