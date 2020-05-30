2200437f74c088e3dfcfc6284051d2972c7bdd10
sethsaperstein <sethsaper@gmail.com>
2020-05-30 14:39:30 -0400

    save changes on attempted pseudo scale

--- bexh-web-app/src/app/components/EventGraph/EventGraph.js
+++ bexh-web-app/src/app/components/EventGraph/EventGraph.js
@@ -4,41 +4,73 @@ import './style.scss';
 export default class EventGraph extends React.Component {
     constructor(props) {
         super(props);
-        this.updateSize = this.updateSize.bind(this);
+        this.state = {
+            "points": {
+                x: [
+                    0,
+                    20,
+                    40,
+                    60,
+                    80,
+                    100,
+                    120,
+                    140
+                ],
+                y: [
+                    0,
+                    60,
+                    80,
+                    20,
+                    80,
+                    60,
+                    100,
+                    90
+                ]
+            }
+        }
+        this.pseduoScalePoints = this.pseduoScalePoints.bind(this);
     }
 
-    // componentDidMount() {
-    //     this.div.addEventListener('resize', this.updateSize);
-    // }
+    pseduoScalePoints(points) {
+        let maxX = Math.max(points["x"]);
+        let maxY = Math.max(points["y"]);
+        let minX = Math.min(points["x"]);
+        let minY = Math.min(points["y"]);
 
-    // componentWillUnmount() {
-    //     this.div.removeEventListener('resize', this.updateSize);
-    // }
+        console.log("div", this.div);
 
-    updateSize() {
-        console.log("SIZE", this.div);
+        let parentX = (typeof this.div?.offsetWidth !== "undefined") ? this.div.offsetWidth : 10;
+        let parentY = (typeof this.div?.offsetHeight !== "undefined") ? this.div.offsetHeight : 10;
+
+        let scaleX = parentX / (maxX - minX);
+        let scaledX = points["x"].map((val, key) => {
+            console.log("scale X", scaleX);
+            console.log("val", val);
+            return (val * scaleX);
+        });
+
+        let scaleY = parentY / (maxY - minY);
+        let scaledY = points["y"].map((val, key) => {
+            return (val* scaleY);
+        });
+
+        console.log("scaled x", scaledX);
+        console.log("scaled y", scaledY);
     }
+    
+    // this.setState({
+    //     viewBoxHeight: this.div.offsetHeight,
+    //     viewBoxWidth: this.div.offsetWidth
+    //   });
 
     render() {
-        this.updateSize();
         return (
             <div ref={div => (this.div = div)} className="eventGraph__container">
-                <svg className="eventGraph__svg" preserveAspectRatio="xMinYMin meet">
+                <svg className="eventGraph__svg">
                     <polyline
-                        className="eventGraph__svg"
                         fill="none"
                         stroke="#0074d9"
-                        points="
-                        00,00
-                        20,60
-                        40,80
-                        60,20
-                        80,80
-                        100,80
-                        120,60
-                        140,100
-                        160,90
-                        "
+                        points={this.pseduoScalePoints(this.state.points)}
                     />
                 </svg>
             </div>
